42809b017d6aab2c76743a8e70e53ba4c4a5f0e0
Nathan Phelps <me@nathanwphelps.com>
2011-06-21 22:07:57 -0500

    Check request type to avoid errors

--- app.js
+++ app.js
@@ -17,77 +17,84 @@ app.post('/commits/new/:token', function(request, response) {
   console.log('Payload: ' + request.body.payload);
   var payload = JSON.parse(request.body.payload);
   for (i = 0; i < payload.commits.length; i++) {
-  	try {
+    try {
         var item = payload.commits[i];
-    	var message = item.message;
-    	var url = item.url;
-    	var id = item.id;
-    	var author = item.author.name;
-    	var xml = '<source_commit><message>' + message + '</message><author>' + author + '</author><commit_id>' + id + '</commit_id><url>' + url + '</url></source_commit>';
-    	console.log('Posting the following to Pivotal Tracker: ' + xml);
-    	
-    	var req = http.request({ host: 'www.pivotaltracker.com', port: 80, method: 'POST', path: '/services/v3/source_commits', headers: {'X-TrackerToken' : token, 'Content-Type' : 'application/xml', 'Content-Length' : xml.length} }, function(res) {
-    	  console.log('PT Response status: ' + res.statusCode);
-    	  console.log('PT Response headers: ' + JSON.stringify(res.headers));
-    	  res.setEncoding('utf8');
-    	  res.on('data', function(chunk) {
-    	    console.log('PT Response body: ' + chunk);
-    	  });
-    	});
-    	
-    	req.on('error', function(e) {
-    	  console.log('An error was encountered: ' + e.message);
-    	});
-    	
-    	req.write(xml);
-    	req.end();
+      var message = item.message;
+      var url = item.url;
+      var id = item.id;
+      var author = item.author.name;
+      var xml = '<source_commit><message>' + message + '</message><author>' + author + '</author><commit_id>' + id + '</commit_id><url>' + url + '</url></source_commit>';
+      console.log('Posting the following to Pivotal Tracker: ' + xml);
+      
+      var req = http.request({ host: 'www.pivotaltracker.com', port: 80, method: 'POST', path: '/services/v3/source_commits', headers: {'X-TrackerToken' : token, 'Content-Type' : 'application/xml', 'Content-Length' : xml.length} }, function(res) {
+        console.log('PT Response status: ' + res.statusCode);
+        console.log('PT Response headers: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+          console.log('PT Response body: ' + chunk);
+        });
+      });
+      
+      req.on('error', function(e) {
+        console.log('An error was encountered: ' + e.message);
+      });
+      
+      req.write(xml);
+      req.end();
     }
-	catch (exception) {
-	  console.log(sys.inspect(exception));
-	}
+  catch (exception) {
+    console.log(sys.inspect(exception));
+  }
   }
   response.send(200);
 });
 
 app.post('/projects/:project/stories/new/:token', function(request, response) {
-  console.log('Full Body: ' + request.body);
   var project = request.params.project;
   var token = request.params.token;
-  var sender = request.body.sender;
-  var from = request.body.from;
-  var subject = request.body.subject;
-  var body = request.body['body-plain'];
   
-  console.log('Sender: ' + sender);
-  console.log('From: ' + from);
-  console.log('Recipient: ' + request.body.recipient);
-  console.log('Subject: ' + subject);
-  console.log('Body: ' + body);
-  console.log('Attachments: ' + request.body['attachment-count']);
-  try {
-    var storyXml = '<story><story_type>feature</story_type><name>' + subject + '</name><requested_by>' + from.replace(/\s*<.*>/, '') + '</requested_by><labels>new</labels><description>' + body + '</description></story>';
-    console.log('Posting the following to Pivotal Tracker: ' + storyXml);
+  if (request.is('*/x-www-form-urlencoded')) {
+  
+    var sender = request.body.sender;
+    var from = request.body.from;
+    var subject = request.body.subject;
+    var body = request.body['body-plain'];
+  
+    console.log('Sender: ' + sender);
+    console.log('From: ' + from);
+    console.log('Recipient: ' + request.body.recipient);
+    console.log('Subject: ' + subject);
+    console.log('Body: ' + body);
+    console.log('Attachments: ' + request.body['attachment-count']);
+    try {
+      var storyXml = '<story><story_type>feature</story_type><name>' + subject + '</name><requested_by>' + from.replace(/\s*<.*>/, '') + '</requested_by><labels>new</labels><description>' + body + '</description></story>';
+      console.log('Posting the following to Pivotal Tracker: ' + storyXml);
     
-    var req = http.request({ host: 'www.pivotaltracker.com', port: 80, method: 'POST', path: '/services/v3/projects/' + project + '/stories', headers: {'X-TrackerToken' : token, 'Content-Type' : 'application/xml', 'Content-Length' : xml.length} }, function(res) {
-      console.log('PT Response status: ' + res.statusCode);
-      console.log('PT Response headers: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      res.on('data', function(chunk) {
-        console.log('PT Response body: ' + chunk);
+      var req = http.request({ host: 'www.pivotaltracker.com', port: 80, method: 'POST', path: '/services/v3/projects/' + project + '/stories', headers: {'X-TrackerToken' : token, 'Content-Type' : 'application/xml', 'Content-Length' : xml.length} }, function(res) {
+          console.log('PT Response status: ' + res.statusCode);
+          console.log('PT Response headers: ' + JSON.stringify(res.headers));
+          res.setEncoding('utf8');
+          res.on('data', function(chunk) {
+            console.log('PT Response body: ' + chunk);
+          });
       });
-    });
-    	
-    req.on('error', function(e) {
-      console.log('An error was encountered: ' + e.message);
-    });
-	
-    req.write(storyXml);
-    req.end();
+      
+      req.on('error', function(e) {
+          console.log('An error was encountered: ' + e.message);
+      });
+  
+      req.write(storyXml);
+      req.end();
+    }
+    catch (exception) {
+      console.log(sys.inspect(exception));
+    }
+    response.send(200);
   }
-  catch (exception) {
-    console.log(sys.inspect(exception));
+  else {
+    console.log('Must be multipart');
+    response.send(200);
   }
-  response.send(200);
 });
 
 var port = process.env.PORT || 3000;
